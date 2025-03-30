5fa0040893427fe7b5120690c932178a2a11117c
Kakapo <kacper20015@o2.pl>
2025-03-30 12:46:38 +0200

    Import and analise data

---
+++ main.py
@@ -0,0 +1,22 @@
+import kagglehub
+import pandas as pd
+import matplotlib.pyplot as plt
+
+df = pd.read_csv("yellow_tripdata_2016-01.csv")
+
+# Display first few rows
+print(df.head())
+
+print(df.isnull().sum())
+
+df["tpep_pickup_datetime"] = pd.to_datetime(df["tpep_pickup_datetime"])
+df["tpep_dropoff_datetime"] = pd.to_datetime(df["tpep_dropoff_datetime"])
+
+print(df["trip_distance"])
+
+df["trip_distance"].hist(bins=50)
+plt.xlabel("Trip Distance (miles)")
+plt.ylabel("Frequency")
+plt.yscale('log')
+plt.title("Distribution of Trip Distances")
+plt.show()
